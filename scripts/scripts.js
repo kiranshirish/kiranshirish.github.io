edbffa317b845cb917ee7f5567e4cb103ad13f8a
kiranshirish <50248598+kiranshirish@users.noreply.github.com>
2025-04-28 12:15:27 -0700

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,3 +10,13 @@ navOpen.addEventListener('click', () => {
 navClose.addEventListener('click', () => {
     nav.style.right = "-20rem";
 })
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QH0LDXLE51"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QH0LDXLE51');
+</script>
