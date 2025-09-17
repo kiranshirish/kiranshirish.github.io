31b3fa86905726d918e3c547679fb960631e1675
kiranshirish <50248598+kiranshirish@users.noreply.github.com>
2025-09-17 12:26:38 -0700

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,38 @@
-// Navbar toggle
 const navOpen = document.querySelector('.fa-bars');
 const navClose = document.querySelector('.fa-x');
 const nav = document.querySelector('.nav-menu');
 
+// Open menu
 navOpen.addEventListener('click', () => {
-  nav.style.right = "0";            // show menu
-  navOpen.style.display = "none";   // hide hamburger
-  navClose.style.display = "block"; // show close button
+  if (window.innerWidth <= 1200) {   // only mobile
+    nav.style.right = "0";
+    navOpen.style.display = "none";
+    navClose.style.display = "block";
+  }else{
+    navOpen.style.display = "none";
+    navClose.style.display = "none";
+  }
 });
 
+// Close menu
 navClose.addEventListener('click', () => {
-  nav.style.right = "-20rem";       // hide menu
-  navOpen.style.display = "block";  // show hamburger
-  navClose.style.display = "none";  // hide close button
-});
-
-document.querySelectorAll('.nav-menu a').forEach(link => {
-  link.addEventListener('click', () => {
-    nav.style.right = "-20rem";     
+  if (window.innerWidth <= 1200) {   // only mobile
+    nav.style.right = "-20rem";
     navOpen.style.display = "block";
     navClose.style.display = "none";
+  }else{
+    navOpen.style.display = "none";
+    navClose.style.display = "none";
+  }
+});
+
+// Close when clicking a link (mobile only)
+document.querySelectorAll('.nav-menu a').forEach(link => {
+  link.addEventListener('click', () => {
+    if (window.innerWidth <= 1200) {
+      nav.style.right = "-20rem";
+      navOpen.style.display = "block";
+      navClose.style.display = "none";
+    }
   });
 });
